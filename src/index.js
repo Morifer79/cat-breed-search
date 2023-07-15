b94f8816ceda11ff9061c9b9443fd9a8fabefc8f
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-07-15 13:37:35 +0300

    update select

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import {fetchBreeds, fetchCatByBreed} from "./js/cat-api";
 import { Report } from 'notiflix/build/notiflix-report-aio';
 import {Loading} from 'notiflix/build/notiflix-loading-aio';
 
-const optionPoint = document.querySelector('.option');
-const breedSelect = document.querySelector('.breed-select'); //selectBtn
+const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 
 fetchBreeds().then(data => {
 	const option = data
 	.map(({id, name}) => `<option class="option" value="${id}">${name}</option>`);
-		breedSelect.innerHTML = option;
+		breedSelect.insertAdjacentHTML('beforeend', option);
 		Loading.remove()})
 	.catch(() => {Report.failure('MEOW!', 'Something went wrong! Try reloading the page!', '😿')});
 
@@ -23,9 +22,9 @@ breedSelect.addEventListener('change', (e) => {
 		const info = `
 		<div class='thumb-pic'><img src="${cat.url}" alt="${cat.id}" width=400></div>
 		<div class='thumb'>
-		<h2>${cat.breeds[0].name}</h2>
-		<p>${cat.breeds[0].description}</p>
-		<p><b>Temperament:</b> ${cat.breeds[0].temperament}</p>
+			<h2>${cat.breeds[0].name}</h2>
+			<p>${cat.breeds[0].description}</p>
+			<p><b>Temperament:</b> ${cat.breeds[0].temperament}</p>
 		</div>`;
 		catInfo.innerHTML = info})
 	.catch(() => {Report.failure('MEOW!', 'Something went wrong! Try reloading the page!', '😿')})});
